37c970195e584065fde233edf79890ffa835c3c3
nasriyasoftware <nasriya.software@outlook.com>
2024-06-07 09:37:59 +0300

    remove dist from repo

--- dist/esm/docs/docs.js
+++
@@ -1,1 +0,0 @@
-export const dnsRecordTypes = ['A', 'AAAA', 'CAA', 'CERT', 'CNAME', 'DINSKEY', 'DS', 'HTTPS', 'LOC', 'MX', 'NAPTR', 'NS', 'PTR', 'SMIMEA', 'SVR', 'SSHFP', 'SVCB', 'TLSA', 'TXT', 'URI'];
